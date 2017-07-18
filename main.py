97cd7aae62f2f6e72eb5d3f5aece3ff2406b3417
srikanthmalla <mallasrikanth004@gmail.com>
2017-07-18 16:01:56 -0400

    autonomous mode tested for track1

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, MaxPooling2D, Convolution2D
+from keras.layers import Flatten, Dense, Lambda, MaxPooling2D, Convolution2D,Cropping2D
 from scripts.reader import X_train, Y_train
 
 model= Sequential()
 model.add(Lambda(lambda x: x/255.0 -0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Convolution2D(6,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5,activation="relu"))
--- scripts/reader.py
+++ scripts/reader.py
@@ -2,26 +2,32 @@ import csv
 import cv2
 import numpy as np
 from scripts.hyperparams import *
+from PIL import Image
 
 lines=[]
 with open(csv_path) as csvfile:
 	reader=csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
-images=[]
-measurements=[]
+car_images=[]
+steering_angles=[]
 for line in lines:
-	source_path=line[0]
-	filename=source_path.split('/')[-1]
-	current_path=base_path+filename
-	image=cv2.imread(current_path)
-	images.append(image)
-	measurement=float(line[3])
-	measurements.append(measurement)
-X_train=np.array(images)
-Y_train=np.array(measurements)
+	img_center = np.asarray(Image.open(base_path + (line[0]).split('/')[-1]))
+	img_left = np.asarray(Image.open(base_path + (line[1]).split('/')[-1]))
+	img_right = np.asarray(Image.open(base_path + (line[2]).split('/')[-1]))
+	# add images and angles to data set
+	car_images.extend((img_center, img_left, img_right))
+	steering_center=float(line[3])
+	# create adjusted steering measurements for the side camera images
+	correction = 0.2 # this is a parameter to tune
+	steering_left = steering_center + correction
+	steering_right = steering_center - correction
+	steering_angles.extend((steering_center, steering_left, steering_right))
+
+X_train=np.array(car_images)
+Y_train=np.array(steering_angles)
 height =np.shape(X_train)[1]
 width =np.shape(X_train)[2]
 
 if __name__=="__main__":
-	print(height,width,Y_train[6])
+	print(height,width,np.shape(X_train),np.shape(Y_train))
