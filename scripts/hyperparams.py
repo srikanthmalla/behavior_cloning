a95967e2a831bc55c83bfff8d0f5c420db926e4d
srikanthmalla <mallasrikanth004@gmail.com>
2017-07-17 22:28:44 -0400

    need to improvise network and preprocessing

---
+++ scripts/hyperparams.py
@@ -0,0 +1,2 @@
+csv_path='./dataset/driving_log.csv'
+base_path='./dataset/IMG/'
--- scripts/reader.py
+++ scripts/reader.py
@@ -1,9 +1,10 @@
 import csv
 import cv2
 import numpy as np
+from scripts.hyperparams import *
 
 lines=[]
-with open('./dataset/driving_log.csv') as csvfile:
+with open(csv_path) as csvfile:
 	reader=csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
@@ -12,14 +13,15 @@ measurements=[]
 for line in lines:
 	source_path=line[0]
 	filename=source_path.split('/')[-1]
-	current_path='./dataset/IMG/'+filename
+	current_path=base_path+filename
 	image=cv2.imread(current_path)
 	images.append(image)
 	measurement=float(line[3])
 	measurements.append(measurement)
 X_train=np.array(images)
 Y_train=np.array(measurements)
+height =np.shape(X_train)[1]
+width =np.shape(X_train)[2]
 
 if __name__=="__main__":
-	print(np.shape(X_train))
-	print(np.shape(Y_train))
+	print(height,width,Y_train[6])
